dd8289fc714ba09f4c483668d2b6143a00c191f1
anastassia <nastaglinka2@gmail.com>
2025-11-24 22:04:11 +0200

    Add interest rate and update BankAccount constructors
    Refactor BankAccount class to include interest rate and constructors.

--- Лабороторна робота 2/BankAccount.java
+++ Лабороторна робота 2/BankAccount.java
@@ -1,31 +1,50 @@
 package ua.opnu;
 
 public class BankAccount {
-    private String accountNumber;
-    private String owner;
     private double balance;
+    private double interestRate;
 
-    public BankAccount(String accountNumber, String owner, double balance) {
-        this.accountNumber = accountNumber;
-        this.owner = owner;
-        this.balance = balance;
+    public BankAccount() {
+        this.balance = 0.0;
+        this.interestRate = 0.0;
     }
 
-    public String getAccountNumber() { return accountNumber; }
-    public String getOwner() { return owner; }
-    public double getBalance() { return balance; }
+    public BankAccount(double balance, double interestRate) {
+        this.balance = balance;
+        this.interestRate = interestRate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
 
     public void deposit(double amount) {
-        balance += amount;
+        if(amount > 0)
+            balance += amount;
     }
 
     public boolean withdraw(double amount) {
-        if (amount <= balance) {
+        if(amount > 0 && amount <= balance) {
             balance -= amount;
             return true;
-        } else {
-            return false;
         }
+        return false;
+    }
+
+    public void addInterest() {
+        balance += balance * interestRate / 100.0;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
     }
 }
 
