cfa568eb087e572bff09275a24c9d9cf36ee4518
anastassia <nastaglinka2@gmail.com>
2025-11-24 22:08:18 +0200

    Allow zero amount for deposit and withdrawal methods

--- Лабороторна робота 2/BankAccount.java
+++ Лабороторна робота 2/BankAccount.java
@@ -23,12 +23,12 @@ public class BankAccount {
     }
 
     public void deposit(double amount) {
-        if(amount > 0)
+        if(amount >= 0)
             balance += amount;
     }
 
     public boolean withdraw(double amount) {
-        if(amount > 0 && amount <= balance) {
+        if(amount >= 0 && amount <= balance) {
             balance -= amount;
             return true;
         }
