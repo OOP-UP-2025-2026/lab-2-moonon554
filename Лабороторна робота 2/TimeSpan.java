226d9346d47a68e88787f122048767d15283c516
anastassia <nastaglinka2@gmail.com>
2025-11-24 22:03:55 +0200

    Update TimeSpan.java

--- Лабороторна робота 2/TimeSpan.java
+++ Лабороторна робота 2/TimeSpan.java
@@ -1,38 +1,44 @@
 package ua.opnu;
 
 public class TimeSpan {
-    private double hours; // используем double для точности дробных часов
+    private int hours;
+    private int minutes;
 
     public TimeSpan() {
         this.hours = 0;
-    }
-
-    public TimeSpan(double hours) {
-        this.hours = hours;
+        this.minutes = 0;
     }
 
     public TimeSpan(int hours, int minutes) {
-        this.hours = hours + minutes / 60.0;
+        this.hours = hours + minutes / 60;
+        this.minutes = minutes % 60;
     }
 
-    public void add(TimeSpan other) {
-        this.hours += other.hours;
-    }
-
-    public void add(double hours) {
-        this.hours += hours;
+    public TimeSpan(int minutes) {
+        this.hours = minutes / 60;
+        this.minutes = minutes % 60;
     }
 
     public int getHours() {
-        return (int) hours;
+        return hours;
     }
 
     public int getMinutes() {
-        return (int) Math.round((hours - getHours()) * 60);
+        return minutes;
+    }
+
+    public void add(int hours, int minutes) {
+        int total = this.hours * 60 + this.minutes + hours * 60 + minutes;
+        this.hours = total / 60;
+        this.minutes = total % 60;
+    }
+
+    public void add(TimeSpan other) {
+        add(other.hours, other.minutes);
     }
 
     @Override
     public String toString() {
-        return getHours() + " год " + getMinutes() + " хв";
+        return hours + " год " + minutes + " хв";
     }
 }
