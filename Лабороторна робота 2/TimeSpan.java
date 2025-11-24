4bb07497dffb81436cdd769e5c7ae2213c8cb019
anastassia <nastaglinka2@gmail.com>
2025-11-24 21:58:03 +0200

    Refactor TimeSpan to use double for hours
    Refactor TimeSpan class to use double for hours and simplify methods.

--- Лабороторна робота 2/TimeSpan.java
+++ Лабороторна робота 2/TimeSpan.java
@@ -1,45 +1,38 @@
-import java.util.Scanner;
+package ua.opnu;
 
 public class TimeSpan {
-    private int hours;
-    private int minutes;
+    private double hours; // используем double для точности дробных часов
 
     public TimeSpan() {
         this.hours = 0;
-        this.minutes = 0;
+    }
+
+    public TimeSpan(double hours) {
+        this.hours = hours;
     }
 
     public TimeSpan(int hours, int minutes) {
-        this.hours = hours + minutes / 60;
-        this.minutes = minutes % 60;
+        this.hours = hours + minutes / 60.0;
     }
 
-    public void addHours(int hours) {
+    public void add(TimeSpan other) {
+        this.hours += other.hours;
+    }
+
+    public void add(double hours) {
         this.hours += hours;
     }
 
-    public void addMinutes(int minutes) {
-        this.hours += minutes / 60;
-        this.minutes += minutes % 60;
+    public int getHours() {
+        return (int) hours;
     }
 
-    public void addTimeSpan(TimeSpan other) {
-        addHours(other.hours);
-        addMinutes(other.minutes);
+    public int getMinutes() {
+        return (int) Math.round((hours - getHours()) * 60);
     }
 
     @Override
     public String toString() {
-        return hours + " год " + minutes + " хв";
-    }
-
-    // Метод для створення TimeSpan через введення користувача
-    public static TimeSpan createFromInput(Scanner sc) {
-        System.out.print("Введіть години: ");
-        int h = Integer.parseInt(sc.nextLine());
-        System.out.print("Введіть хвилини: ");
-        int m = Integer.parseInt(sc.nextLine());
-        return new TimeSpan(h, m);
+        return getHours() + " год " + getMinutes() + " хв";
     }
 }
-
